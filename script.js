0423989aa6229cecab4b4be17f5faafd6160c415
Christie Tanjaya <christie.tanc@gmail.com>
2024-07-26 14:47:37 +0800

    moving devices

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //document.getElementById("D4").children[1].textContent = "test"; //test getting the text inside the svg
+
+const availableDice = [D4:4,D6:6,D8:8,D10:10,D12:12,D20:20]
+
+function rollDice (dice)
+{
+    
+}
+
+console.log(availableDice[1]);
